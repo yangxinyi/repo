4a4fbe1e656e6069e0b64667d29fca8a2efd183b
yangxinyi <xin3336019@sina.com>
2013-03-19 23:27:14 +0800

    debug

--- androidspace/app_test/src/com/app_test/MainActivity.java
+++ androidspace/app_test/src/com/app_test/MainActivity.java
@@ -1,11 +1,12 @@
 package com.app_test;
 
+import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -14,21 +15,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Log.i("8023","loginfo");
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
         if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
         	System.out.println("enter gps_provider");
 	        Location location = locationManager  
 	        .getLastKnownLocation(LocationManager.GPS_PROVIDER);  
 	        if(location != null){  
-	        	printLocation(location);
+	        	System.out.println("gps:" + location.getLatitude() + " , " + location.getLongitude());
 	        }  
         }else{  
         	System.out.println("enter network_provider");
 	        LocationListener locationListener = new LocationListener() {  
 		        public void onLocationChanged(Location location) { //当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发  
 			        if (location != null) {  
-			        	printLocation(location);
+			        	System.out.println("network:" + location.getLatitude() + " , " + location.getLongitude());
 			        }  
 		        }  
 		        public void onProviderDisabled(String provider) {  
@@ -43,12 +44,6 @@ public class MainActivity extends Activity {
 	        };  
 	        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000, 0,locationListener); 
        }
-    }
-
-    private void printLocation(Location location) {
-    	System.out.println("Location changed : Lat: "  
-		        + location.getLatitude() + " Lng: "  
-		        + location.getLongitude());
     }
 
     @Override
