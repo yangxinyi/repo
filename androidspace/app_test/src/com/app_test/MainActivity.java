13cb982dee2cb40e1fccbc8a440a8a92cc558441
yangxinyi <xin3336019@sina.com>
2013-03-20 14:57:53 +0800

    gps location success

--- androidspace/app_test/src/com/app_test/MainActivity.java
+++ androidspace/app_test/src/com/app_test/MainActivity.java
@@ -2,55 +2,82 @@ package com.app_test;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.i("8023","loginfo");
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-        	System.out.println("enter gps_provider");
-	        Location location = locationManager  
-	        .getLastKnownLocation(LocationManager.GPS_PROVIDER);  
-	        if(location != null){  
-	        	System.out.println("gps:" + location.getLatitude() + " , " + location.getLongitude());
-	        }  
-        }else{  
-        	System.out.println("enter network_provider");
-	        LocationListener locationListener = new LocationListener() {  
-		        public void onLocationChanged(Location location) { //当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发  
-			        if (location != null) {  
-			        	System.out.println("network:" + location.getLatitude() + " , " + location.getLongitude());
-			        }  
-		        }  
-		        public void onProviderDisabled(String provider) {  
-		        // Provider被disable时触发此函数，比如GPS被关闭  
-		        }  
-		        public void onProviderEnabled(String provider) {  
-		        // Provider被enable时触发此函数，比如GPS被打开  
-		        }  
-		        public void onStatusChanged(String provider, int status, Bundle extras) {  
-		        // Provider的转态在可用、暂时不可用和无服务三个状态直接切换时触发此函数  
-		        }  
-	        };  
-	        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000, 0,locationListener); 
-       }
-    }
+	private TextView textLatitude;
+	private TextView textLongitude;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	private LocationListener locationListener = new LocationListener() {
+
+		// 当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
+		public void onLocationChanged(Location location) {
+			if (location != null) {
+				textLatitude.setText("Latitude2:" + location.getLatitude());
+				textLongitude.setText("Longitude2:" + location.getLongitude());
+			}
+		}
+
+		public void onProviderDisabled(String provider) {
+			// Provider被disable时触发此函数，比如GPS被关闭
+		}
+
+		public void onProviderEnabled(String provider) {
+			// Provider被enable时触发此函数，比如GPS被打开
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// Provider的转态在可用、暂时不可用和无服务三个状态直接切换时触发此函数
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		textLatitude = (TextView) findViewById(R.id.lati_str);
+		textLongitude = (TextView) findViewById(R.id.longi_str);
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		findBestProvider(locationManager);
+		
+		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			Location location = locationManager
+					.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			if (location != null) {
+				System.out.println("last gps:" + location.getLatitude() + " , "
+						+ location.getLongitude());
+				textLatitude.setText("Latitude1:" + location.getLatitude());
+				textLongitude.setText("Longitude1:" + location.getLongitude());
+			}
+			locationManager.requestLocationUpdates(
+					LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+		}
+	}
+
+	private String findBestProvider(LocationManager locationManager) {
+		Criteria criteria = new Criteria();  
+	    criteria.setAccuracy(Criteria.ACCURACY_FINE);// 高精度  
+	    criteria.setCostAllowed(false);// 设置允许产生资费  
+	    criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);// 低功耗  
+	
+	    String provider = locationManager.getBestProvider(criteria, true);// 获取GPS信息
+	    System.out.println("best provider is " + provider);
+	    return provider;
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
